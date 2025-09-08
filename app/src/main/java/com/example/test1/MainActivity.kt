c7863ad930e731670492227b728dee3571ea2fa7
korczn <korczyyn@gmail.com>
2025-09-08 14:13:35 +0200

    Poprawiono funkcjonalność strony

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.test1
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-}
+
+//        val showDatePickerButton: Button = findViewById(R.id.open_datepicker_button)
+//        showDatePickerButton.setOnClickListener {
+//            showDatePickerDialog()
+//
+//            val timePicker = findViewById<TimePicker>(R.id.spinner_timepicker)
+//            timePicker.setIs24HourView(true)
+//
+//            timePicker.setOnTimeChangedListener { view, hour, minute ->
+//                val selectedTime = "$hour:$minute"
+//                view.setBackgroundColor(Color.CYAN)
+//                Toast.makeText(this, "Wybrana godzina: $selectedTime", Toast.LENGTH_SHORT).show()
+            }
+
+        }
